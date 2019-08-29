9a780afa053fb51d81fced0550535e69917f6de7
Nicholas Leone <nick.al.leone@gmail.com>
2019-08-29 11:33:05 -0700

    Updating files and adding controllers for handlebars

---
+++ public/js/view.js
@@ -0,0 +1,4 @@
+$("#view-all").on("click", function(){
+    console.log("The 'view-all' button was clicked!")
+    
+})
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require("express");
+var exphbs = require("express-handlebars");
 
 var app = express();
 var PORT = process.env.PORT || 8080;
@@ -10,10 +11,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 // require("./routes/html-routes.js")(app);
 require("./routes/product-api-routes.js")(app);
 require("./routes/account-api-routes")(app);
 require("./routes/admin-api-routes")(app);
+
+var routes = require("./controllers/viewsController.js");
+
+app.use(routes)
 
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
